db1095d2f7979c322a5aaeb48dc47924e3403888
sameer302 <sameernilkhan11@gmail.com>
2025-06-11 15:58:10 +0530

    revised till operators

---
+++ C_Programming/cascading_of_increment_operator.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+    int x, y = 5, z = 6;
+    x = y+++y;
+    printf("%d",x);
+}
---
+++ C_Programming/cyclicordertest.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main(){
+    int x = (300*300)/300;
+    printf("%d\n",x);
+    printf("%d",sizeof(x));
+}
