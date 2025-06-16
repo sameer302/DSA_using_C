be1dafe576c9906323e8954c8613fe40120b4926
sameer302 <sameernilkhan11@gmail.com>
2025-06-16 18:17:06 +0530

    going forward

---
+++ C_Programming/scanf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int x, y, z;
+    z = scanf("%d %d", &x, &y);
+    printf("%d %d %d",x , y, z); 
+    /*int x,y;
+    scanf("%o",&x); // here input should be an octal number
+    printf("%d",x);
+    printf("100",100);*/
+}
