e33897fad57ad9cd5e88817911812dadd3e7b122
sameer302 <sameernilkhan11@gmail.com>
2025-06-17 18:16:27 +0530

    understood about loops

---
+++ C_Programming/switch.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    int x = 5;
+    switch(x){
+        case 1:
+        printf("c1");
+        break;
+
+        default:
+        printf("d");
+
+        case 6:
+        printf("c5");
+        break;
+    }
+}
